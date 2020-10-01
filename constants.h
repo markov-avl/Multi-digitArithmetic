1de24c0f88690b07e5d81c03d73e60a00ae6ddc8
markov-avl <andrewshasucks@gmail.com>
2020-10-01 19:22:16 +1000

    file paths change

--- constants.h
+++ constants.h
@@ -1,8 +1,8 @@
 #ifndef LAB1_CONSTANTS_H
 #define LAB1_CONSTANTS_H
 
-const char* IN_PATH = "C:/Users/Andrewsha/CLionProjects/Lab1/workfiles/infile.txt";
-const char* OUT_PATH = "C:/Users/Andrewsha/CLionProjects/Lab1/workfiles/outfile.txt";
+const char* IN_PATH = "workfiles/infile.txt";
+const char* OUT_PATH = "workfiles/outfile.txt";
 const short DATA_SIZE = 100;
 const short BUFF_SIZE = DATA_SIZE * 2;
 
